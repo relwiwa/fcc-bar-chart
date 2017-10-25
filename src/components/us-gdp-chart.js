5fe34255992b9e44751e0174e16501cddbf6ef2b
Walter <relw1wa-github@yahoo.de>
2017-10-25 11:49:05 +0200

    Fix initial display of GDP datum

--- src/components/us-gdp-chart.js
+++ src/components/us-gdp-chart.js
@@ -131,7 +131,6 @@ class UsGdpChart extends Component {
         minXValue: minXValue,
       },
       progressMessage: null,
-      currGdpDatum: preparedData[0],
       preparedData: preparedData,
     });
   };
