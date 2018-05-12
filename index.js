3eabcc2698ea78d7701b8a0e28d268036cf2a9eb
Walter <relw1wa-github@yahoo.de>
2018-05-12 21:34:53 +0200

    Refactor folder structure and build process

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ es6Promise.polyfill();
 import React from 'react';
 import { render } from 'react-dom';
 
-import './global-styles.scss';
+import '../../../styles/global-styles.scss';
 import UsGdpChart from './components/us-gdp-chart';
 
 render(
